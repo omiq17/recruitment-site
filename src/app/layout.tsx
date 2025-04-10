0b7fcb732f5e8683d153bf07ec16b85f53e23afa
omiq17 <omiq17@gmail.com>
2025-04-10 17:39:07 +0800

    Add vercel analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Roboto } from "next/font/google";
 import "@/styles/globals.css";
 import "keen-slider/keen-slider.min.css";
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <BootstrapClient />
+        <Analytics />
       </body>
     </html>
   );
